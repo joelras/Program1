0bdd974047975a91db80dd4238f9095e1a727def
joelr <joelr@Litos-Laptop.fios-router.home>
2023-01-25 22:23:58 -0500

    initial project commit

---
+++ JavaProgram/src/catclient/TestCat.java
@@ -0,0 +1,8 @@
+package catclient;
+
+public class TestCat{
+	public static void main (String[] args) {
+		Cat prince = new Cat();
+		Cat betty = new Cat("Betty");
+	}
+}
---
+++ JavaProgram/src/package1/program1.java
@@ -0,0 +1,7 @@
+package package1;
+
+public class program1 {
+	public static void main(String[] args) {
+		System.out.println("First Eclipse Java Program");
+	}
+}
